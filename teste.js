a7046850c3fcaf52886e66848a3d6999f9026b89
Hugo Ribeiro <108608507+xHuGODx@users.noreply.github.com>
2026-04-27 18:24:19 +0100

    Use execFile instead of exec for command execution
    Replaced exec with execFile for improved security.

--- teste.js
+++ teste.js
@@ -4,7 +4,7 @@ function pingServer(userIP) {
 // DANGER: Taking untrusted user input and putting it directly into a system command!
 // An attacker could pass an IP like: "8.8.8.8; rm -rf /"
   const command = "ping -c 4 " + userIP;
-  exec(command, (error, stdout, stderr) => {
+  execFile(command, (error, stdout, stderr) => {
     console.log(stdout);
   });
 }
